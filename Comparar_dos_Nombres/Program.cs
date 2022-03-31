c8ce510c6396829cae6d919db9803ea14140d1a4
Umblum12 <68798247+Umblum12@users.noreply.github.com>
2022-03-31 00:36:31 -0600

    Agregar archivos de proyecto.

---
+++ Comparar_dos_Nombres/Program.cs
@@ -0,0 +1,18 @@
+﻿string nombre1;
+string nombre2;
+
+Console.WriteLine("Ingrese el Primer Nombre");
+nombre1 = Console.ReadLine();
+
+Console.WriteLine("Ingrese el Segundo Nombre");
+nombre2 = Console.ReadLine();
+
+if (nombre1 == nombre2)
+{
+    Console.WriteLine("--- Los Nombres son Iguales ---");
+}
+else
+{
+    Console.WriteLine("--- Los Nombres son Diferentes ---");
+}
+Console.ReadKey(true);
